097b526a0cbca2b72b6fc31c25a5cca7e446cc40
Vladimir Lekhterev <vovalekhterev@gmail.com>
2025-12-15 18:50:33 +0300

    Implement API for ONBExplorer

---
+++ api/apiclient.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <array>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace API
+{
+
+struct Client final
+{
+public:
+  using Samples = std::vector<std::vector<char>>;
+private:
+  struct Impl;
+  std::unique_ptr<Impl> pimpl_;
+
+  std::array<char, 1024> buf_;
+
+private:
+  void     setPacketSize(uint16_t sz);
+  uint16_t getPacketSize();
+  uint16_t readPacket();
+
+public:
+  Client(const std::string& addr = "127.0.0.1", size_t port=4242);
+  
+  ~Client();
+  Client(Client&&);
+  Client& operator=(Client&&);
+
+  std::string getParamStr(const std::string& dev, const std::string& param);
+  void setParamStr(const std::string& dev, const std::string& param, const std::string& value);
+  std::string invokeStr(const std::string& dev, const std::string& param, const std::string& value="");
+
+  template<typename T>
+  T getParam(const std::string& dev, const std::string& param);
+
+  template<typename T>
+  void setParam(const std::string& dev, const std::string& param, const T& value);
+
+  std::vector<std::string> listDevices();
+  std::vector<std::string> listParams(const std::string& dev);
+
+  void setAGRequest(const std::string& dev, uint16_t interval, const std::vector<std::string>& params);
+
+  Samples getSamples();
+};
+
+}
---
+++ api/apicommon.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <QTcpSocket>
+#include <cassert>
+
+namespace API
+{
+
+int readAll(QTcpSocket* psock, char* buf, size_t sz);
+int peekAll(QTcpSocket* psock, char* buf, size_t sz);
+int writeAll(QTcpSocket* psock, const char* buf, size_t sz);
+
+enum MessageT: char
+{
+  // <-- | sz | op | iparam | dev + \0 | param + \0 |
+  // --> | sz | op | value + \0 |
+  GetParam = 'r',
+  
+  // <-- | sz | op | iparam | ivalue | dev + \0 | param + \0 | value + \0 |
+  // --> | sz | op |
+  SetParam = 'w',
+  
+  // <-- | sz | op |
+  // --> | sz | op | [dev\0]* | \0 |
+  ListDevices = 'd',
+
+  // <-- | sz | op | dev + \0 |
+  // --> | sz | op | [param\0]* | \0 |
+  ListParams = 'p',
+
+  // <-- | sz | op | iparam | ivalue | dev + \0 | param + \0 | value + \0 |
+  // --> | sz | op | value + \0 |
+  Invoke = 'i',
+
+  // <-- | sz | op | devlen | dev + \0 | int | [param + \0]* | \0 |
+  // --> | sz | op |
+  SetAGRequest = 'a',
+
+  // <-- | sz | op |
+  // --> | sz | op | nsamples |
+  // <-- | sz | op | sample | (nsamples times)
+  GetSamples = 's',
+  
+  // <-- | sz | op | msg + \0 |
+  Error = 'e'
+
+
+};
+
+struct AnalyzerSample
+{
+  float freq;
+  float magn;
+  float phase;
+  float rms;
+} __attribute__((packed));
+
+}
